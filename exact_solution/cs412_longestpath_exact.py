dadc8a81b1971ea023d6e2906f3725d84e09ab58
[Patrick Belbin] <113935201+belbinpr@users.noreply.github.com>
2024-11-21 10:49:46 -0500

    reordered files, fixed readme, continued exact sol

--- exact_solution/cs412_longestpath_exact.py
+++ exact_solution/cs412_longestpath_exact.py
@@ -24,6 +24,17 @@ list of verticies on path
 def main():
   lenV = int(input())
   lenE = int(input())
+  g = [input().split() for _ in range(lenE)] # list: {{u, v, w}, {u1, u2, uw}}
+  graph = dict()
+  for e in g: # use set to prevent duplicates
+    graph[e[0]] = set()
+    graph[e[1]] = set()
+  for e in g: # dict: {u : (v, w)}, {u : (v, w)} ...
+    graph[e[0]] = ((e[1], e[2]))
+    graph[e[1]] = ((e[0], e[2]))
+
+
+
   return
 
 
