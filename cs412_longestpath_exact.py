457d33a9f721952192aeec4c99224b5e8b9d48ad
[Patrick Belbin] <113935201+belbinpr@users.noreply.github.com>
2024-11-21 10:30:01 -0500

    start of exact file

---
+++ cs412_longestpath_exact.py
@@ -0,0 +1,31 @@
+"""
+    Name: Patrick Belbin
+      Longest Path Exact Solution
+"""
+import itertools as it
+
+""" Example input:
+3 3
+a b 3
+b c 4
+a c 5
+as 
+vertices edges
+v1 v2 w
+v1 v2 w
+Example output:
+9
+a c b
+as 
+length
+list of verticies on path
+"""
+
+def main():
+  lenV = int(input())
+  lenE = int(input())
+  return
+
+
+if __name__ == "__main__":
+  main()
